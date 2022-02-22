dee6bc94a2ee005ee2a985b0594f4be00cc4466e
jhu04 <54165683+jhu04@users.noreply.github.com>
2022-02-22 01:15:48 -0800

    v1.0
    Lists recently downloaded files in a popup

---
+++ popup.js
@@ -0,0 +1,34 @@
+let downloads_list = document.getElementById('downloads_list')
+
+chrome.downloads.search({}, function(res) {
+    let i = 0
+    let cnt = 0
+    
+    while (cnt <= 5)
+    {
+        if (res[i].error !== undefined)
+        {
+            ++i
+            continue
+        }
+
+        let path = res[i].filename
+        console.log(res[i])
+
+        // https://stackoverflow.com/questions/423376/how-to-get-the-file-name-from-a-full-path-using-javascript
+        let filename = path.replace(/^.*[\\\/]/, '')
+
+        // <li><a href="file://${path}" target="_blank">${filename}</a></li> is not allowed because cannot access
+        // local files, but may be useful for input type=file
+        chrome.downloads.open(res[i].id)
+
+        // opening.then(() => {console.log('opened')}, (error) => {console.log('error')})
+        downloads_list.innerHTML += `<li>${filename}</li>`
+
+        ++i
+        ++cnt
+    }
+
+    console.log(res)
+})
+
